59b1780fd9c0819a253b4b086f8f821fd11df5a8
matteus-henryk <matteus.henryk2@gmail.com>
2020-08-08 10:58:44 -0300

    primeiro commit

---
+++ src/utils/format.js
@@ -0,0 +1,44 @@
+//dados
+const subjects = [
+ 
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação Física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Português",
+  "Química",
+]
+
+const weekdays = [
+ 
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+]
+
+// funcionalidades 
+
+function convertHoursToMinutes(time) {
+  const [hour, minutes] = time.split(":")
+  return Number((hour * 60) + minutes)
+}
+
+function getSubject(subjectNumber) {
+  const position = +subjectNumber -1
+  return subjects[position]
+}
+
+module.exports = {
+  subjects,
+  weekdays,
+  getSubject,
+  convertHoursToMinutes
+}
